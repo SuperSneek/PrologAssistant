d77ca588e1ee375bae46430ea0ef8f3cc7d4e8cf
jonas lewandrowski <joecarverde@gmail.com>
2022-04-10 14:21:23 +0200

    Create CLI

--- src/main/java/CommandLineInterface.java
+++ src/main/java/CommandLineInterface.java
@@ -1,2 +1,17 @@
+import java.io.Console;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class CommandLineInterface {
+
+    public static void main(String[] args) {
+        Console console = System.console();
+        Scanner scanner = new Scanner(console.reader());
+        boolean running = true;
+        while (running) {
+            String input = scanner.next();
+            running = false;
+        }
+    }
+
 }
