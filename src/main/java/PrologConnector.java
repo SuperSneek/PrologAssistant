2d6d15141d17debbf34dc64951166365e8b1e185
jonas lewandrowski <joecarverde@gmail.com>
2022-04-12 20:59:13 +0200

    Added voice connection

--- src/main/java/PrologConnector.java
+++ src/main/java/PrologConnector.java
@@ -1,7 +1,19 @@
 import org.jpl7.JPL;
+import org.jpl7.Query;
+import org.jpl7.Term;
 
 public class PrologConnector {
 
-    
+    public PrologConnector() {
+        Term whereRule1 = Term.textToTerm("where(\"Where\"|X))");
+        Term whereRule2 = Term.textToTerm("where(Y|X)) :- where(X)");
+    }
+
+    public void ProcessCommand(String[] words) {
+        if(words[1].toUpperCase().equals("WHERE")) {
+            Term goal = Term.textToTerm("where(X)");
+            Query q = new Query(goal);
+        }
+    }
 
 }
